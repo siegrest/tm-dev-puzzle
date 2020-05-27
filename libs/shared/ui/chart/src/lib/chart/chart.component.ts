2d937a1d98544924c0102b3e02fc9ac1a8b109f0
siegrest <179900@ttu.ee>
2020-05-27 08:37:04 +0300

    chore(stock): Simplify chart data input

--- libs/shared/ui/chart/src/lib/chart/chart.component.ts
+++ libs/shared/ui/chart/src/lib/chart/chart.component.ts
@@ -1,19 +1,16 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'coding-challenge-chart',
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements OnInit, OnDestroy {
-  @Input() data$: Observable<[string | number][]>;
-  private chartDataChange: Subscription;
+export class ChartComponent implements OnInit {
+  @Input() data: [string | number][];
 
   chart = {
     title: 'Stocks chart',
     type: 'LineChart',
-    data: [],
     columnNames: ['period', 'close'],
     options: { title: `Stock price`, width: '600', height: '400' }
   };
@@ -21,14 +18,5 @@ export class ChartComponent implements OnInit, OnDestroy {
   constructor() {}
 
   ngOnInit() {
-    this.chartDataChange = this.data$
-      ? this.data$.subscribe(newData => (this.chart.data = newData))
-      : null;
-  }
-
-  ngOnDestroy() {
-    if (this.chartDataChange) {
-      this.chartDataChange.unsubscribe();
-    }
   }
 }
